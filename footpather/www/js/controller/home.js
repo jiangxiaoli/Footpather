8c4e81aae9edaad99d44ead20fea7f9c1a6fdb90
Xiaoli Jiang <jiangxiaoli1104@gmail.com>
2015-08-24 00:10:14 -0700

    pop user/report/crimereport data on map

--- footpather/www/js/controller/home.js
+++ footpather/www/js/controller/home.js
@@ -11,9 +11,10 @@ angular.module('app.controller.home', [])
     tile.stub();
     map.stub();
     crimereport.stub();
+    getCurrLoc();
 
     //preload all images, pay attention to the performance
-    $scope.streetViewUrl = map.getStreetView(37.777052, -122.403783);
+    //$scope.streetViewUrl = map.getStreetView(37.777052, -122.403783);
 
     //styling of maps
     var styleArray = [ //any style array defined in the google documentation you linked
@@ -43,7 +44,14 @@ angular.module('app.controller.home', [])
       if(!$scope.map) {
         return;
       }
+      getCurrLoc();
+    };
 
+    /**
+     * Get user current location
+     */
+     //TODO update when map drags, change to smaller range...
+    function getCurrLoc(){
       $scope.loading = $ionicLoading.show({
         content: 'Getting current location...',
         showBackdrop: false
@@ -51,11 +59,70 @@ angular.module('app.controller.home', [])
 
       navigator.geolocation.getCurrentPosition(function(pos) {
         $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-        $ionicLoading.hide();
+        $scope.currlat = pos.coords.latitude;
+        $scope.currlng = pos.coords.longitude;
+        getData($scope.currlat, $scope.currlng);
       }, function(error) {
         alert('Unable to get location: ' + error.message);
       });
-    };
+    }
+
+    /**
+     * Get all data
+     * @param lat
+     * @param lng
+     */
+    function getData(lat, lng){
+      //get map center
+      getNearByUsers(lat, lng);
+      getNearByCrimeReports(lat, lng);
+      getNearByReports(lat, lng);
+    }
+
+    /**
+     * Get nearby users
+     * @param lat
+     * @param lng
+     */
+    function getNearByUsers(lat, lng) {
+      tile.getNearByUsers(lat, lng)
+        .success(function(res){
+          $scope.users = res;
+          console.log($scope.users);
+        }).error(function(err){
+          alert("Get Nearby User Error: " + err);
+        });
+    }
+
+    /**
+     * Get nearby reports
+     * @param lat
+     * @param lng
+     */
+    function getNearByReports(lat, lng) {
+      tile.getNearByReports(lat, lng)
+        .success(function(res){
+          $scope.reports = res;
+        }).error(function(err){
+          alert("Get Nearby Report Error: " + err);
+        });
+    }
+
+    /**
+     * Get nearby crimereports
+     * @param lat
+     * @param lng
+     */
+    function getNearByCrimeReports (lat, lng) {
+      tile.getNearByCrimeReports(lat, lng)
+        .success(function(res){
+          $scope.crimereports = res;
+          console.log($scope.crimereports);
+          $ionicLoading.hide(); //takes longest time, hide loading after get crimereport complete
+        }).error(function(err){
+          alert("Get Nearby Crime Error: " + err);
+        });
+    }
 
   }
 );
